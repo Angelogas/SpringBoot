86a61839708ab6dfb6a03540485d507064a8489d
Gastran Njofang <angelogastran@gmail.com>
2021-10-14 10:16:47 -0500

    Rest-API accessing specific Employee endpoint using @PathVariable

--- src/main/java/com/gigacedi/employeesystemmanagement/DemoRestAPI/DemoRestAPIController.java
+++ src/main/java/com/gigacedi/employeesystemmanagement/DemoRestAPI/DemoRestAPIController.java
@@ -2,9 +2,11 @@ package com.gigacedi.employeesystemmanagement.DemoRestAPI;
 
 import com.gigacedi.employeesystemmanagement.Entity.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,16 +14,29 @@ import java.util.List;
 @RequestMapping ("/test")
 public class DemoRestAPIController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World";
+//    @GetMapping("/hello")
+//    public String sayHello() {
+//        return "Hello World";
+//    }
+//
+
+    private List<Employee> theEmployees;
+
+    @PostConstruct
+    public void loadData() {
+
+        theEmployees = new ArrayList<>();
+        theEmployees.add(new Employee("Gastran","Angelo"));
+        theEmployees.add(new Employee("Cedrico","Armand"));
+        theEmployees.add(new Employee("Imelda","Ngondah"));
     }
 
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
-        List<Employee> theEmployees = new ArrayList<>();
-        theEmployees.add(new Employee("Gastran","Angelo"));
-        theEmployees.add(new Employee("Cedrico","Armand"));
         return theEmployees;
+    }
+    @GetMapping("/employees/{employeeId}")
+    public Employee getStudent (@PathVariable int employeeId) {
+        return theEmployees.get(employeeId);
     }
 }
